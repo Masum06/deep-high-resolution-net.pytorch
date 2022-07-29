f9ec197edec6a30cc386a05a6ace7de7482d7c51
Masum Hasan <youngladesh@gmail.com>
2022-07-29 18:24:32 -0400

    Coco 17 coord changed intermediate

--- lib/dataset/coco.py
+++ lib/dataset/coco.py
@@ -48,8 +48,9 @@ class COCODataset(JointsDataset):
         16: "right_ankle"
     },
 	"skeleton": [
-        [16,14],[14,12],[17,15],[15,13],[12,13],[6,12],[7,13], [6,7],[6,8],
-        [7,9],[8,10],[9,11],[2,3],[1,2],[1,3],[2,4],[3,5],[4,6],[5,7]]
+        [1, 2], [1, 6], [1, 10], [1, 14], [1, 18], [2, 6], [6, 10], [10, 14], 
+        [2, 3], [3, 4], [4, 5], [6, 7], [7, 8], [8, 9], [10, 11], [11, 12], 
+        [12, 13], [14, 15], [15, 16], [16, 17], [18, 19], [19, 20], [20, 21]]
     '''
     def __init__(self, cfg, root, image_set, is_train, transform=None):
         super().__init__(cfg, root, image_set, is_train, transform)
@@ -88,16 +89,16 @@ class COCODataset(JointsDataset):
         logger.info('=> num_images: {}'.format(self.num_images))
 
         self.num_joints = 21
-        self.flip_pairs = [[1, 2], [3, 4], [5, 6], [7, 8],
-                           [9, 10], [11, 12], [13, 14], [15, 16]]
+        self.flip_pairs = []
         self.parent_ids = None
-        self.upper_body_ids = (0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
-        self.lower_body_ids = (11, 12, 13, 14, 15, 16)
+        self.upper_body_ids = ()
+        self.lower_body_ids = ()
 
         self.joints_weight = np.array(
             [
                 1., 1., 1., 1., 1., 1., 1., 1.2, 1.2,
-                1.5, 1.5, 1., 1., 1.2, 1.2, 1.5, 1.5
+                1.5, 1.5, 1., 1., 1.2, 1.2, 1.5, 1.5,
+                1., 1., 1., 1.
             ],
             dtype=np.float32
         ).reshape((self.num_joints, 1))
